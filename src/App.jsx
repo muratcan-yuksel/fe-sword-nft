bc70fa8ddbe05491e338b8b683b13eb7ba854658
muratcan-yuksel <muratcanyukselpro@gmail.com>
2024-06-19 14:44:53 +0700

    .

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import WalletComponent from "./components/ConnectWallet";
+
+const App = () => {
+  return (
+    <div>
+      <WalletComponent />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { config } from "../config.js";
+import "./index.css";
+
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <div
+        // style={{
+        //   background:
+        //     "linear-gradient(90deg, rgba(104, 110, 255, 1) 0%, rgba(141, 198, 255, 1) 100%)",
+        //   height: "100vh",
+        //   width: "100vw",
+        // }}
+        >
+          <App />
+        </div>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+);
